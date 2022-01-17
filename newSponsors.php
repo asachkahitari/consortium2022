c0be1418db4ac1f5766975c2ee24ade15ae763ae
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-17 21:48:48 +0530

    Update newSponsors.php

--- newSponsors.php
+++ newSponsors.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Consortium 22 | Sponsors</title>
+    <title>Sponsors | Consortium 22 | E-CELL, VNIT</title>
 </head>
 <?php include('includes/header.php')?>
 <body>
@@ -13,67 +13,67 @@
       <span><img class="hex" src="img/hex.png" alt=""></span>
    
       <div class="row align-items-center container1 text-center mx-auto" >
-        <div class="col-12 col-md-6 col-lg-4 card">
+        <div class="col-12 col-md-6 col-lg-4 card mx-4">
           <img src="" alt="">
         </div>
-        <div class="col-12 col-md-6 col-lg-4 card">
+        <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card ">
+       <div class="col-12 col-md-6 col-lg-4 card  mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
-       <div class="col-12 col-md-6 col-lg-4 card">
+       <div class="col-12 col-md-6 col-lg-4 card mx-4">
          <img src="" alt="">
        </div>
       </div>
@@ -84,7 +84,7 @@
      <h2>Contact Us</h2>
      <p>For any sponsorship related queries, do reach out to us</p>
      <div class="row align-items-center">
-      <div class="col-12 col-md-6 col-lg-4 contactcard">
+      <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">Bharghav Choudhary</h5>
         <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
@@ -96,7 +96,7 @@
         <div class="phone">+91 9836726382</div>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 contactcard">
+      <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">Bharghav Choudhary</h5>
         <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
@@ -107,7 +107,7 @@
         <div class="name">bharghav@gmail.com</div>
         <div class="phone">+91 9836726382</div>
       </div>
-      <div class="col-12 col-md-6 col-lg-4 contactcard">
+      <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">Bharghav Choudhary</h5>
         <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
