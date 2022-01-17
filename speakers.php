ad3fed4d6294d15ee399f449f047f7ae2b0acbed
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-17 21:51:33 +0530

    Update speakers.php

--- speakers.php
+++ speakers.php
@@ -5,68 +5,69 @@
 </head>
 <?php include('includes/header.php')?>
 <body style="background-color: #292929;">
+<hr style="border-top: 1px solid #fff;opacity: 0.8;margin-top: 3px;">
   <h1 class="text-white text-center mt-5" style="font-weight: bold; ">SPEAKERS</h1>
-
-  <span><img class="hexa" src="img/hex.png" alt="" width="450px" style="position: relative;bottom: 200px;left: 250px;opacity: 0.8;margin-bottom: -550px;z-index: -1;"></span>
-
+ 
+  <marquee behavior="" direction="" scrollamount="10" scrolldelay="60" style="position: relative;bottom: 280px;opacity: 0.8;margin-bottom: -550px;z-index: -1;"><span><img src="img/hex.png" alt="" width="450px" ></span></marquee>
+  
   <div class="row align-items-center container1 text-center mx-auto" >
-    <div class="col-12 col-md-6 col-lg-4 speakercard">
+    <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
       <img src="img/karan.jpg" alt="">
       <h5 class="mt-2">KARAN SHAH</h5>
         <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
         <h6 class="my-2 pb-3 desig">Founder & CEO IIDE</h6>
     </div>
 
-    <div class="col-12 col-md-6 col-lg-4 speakercard">
+    <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
           <h6 class="my-2 desig pb-3">Founder & CEO IIDE</h6>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 speakercard">
+      <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
           <h6 class="my-2 desig pb-3">Founder & CEO IIDE</h6>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 speakercard">
+      <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
           <h6 class="my-2 desig pb-3">Founder & CEO IIDE</h6>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 speakercard">
+      <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
           <h6 class="my-2 desig pb-3">Founder & CEO IIDE</h6>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 speakercard">
+      <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
           <h6 class="my-2 desig pb-3">Founder & CEO IIDE</h6>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 speakercard">
+      <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
           <h6 class="my-2 desig pb-3">Founder & CEO IIDE</h6>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 speakercard">
+      <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
           <h6 class="my-2 desig pb-3">Founder & CEO IIDE</h6>
       </div>
 
-      <div class="col-12 col-md-6 col-lg-4 speakercard">
+      <div class="col-12 col-md-6 col-lg-4 speakercard mx-4">
         <img src="img/karan.jpg" alt="">
         <h5 class="mt-2">KARAN SHAH</h5>
           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
@@ -80,7 +81,7 @@
     <h2>Contact Us</h2>
     <p>For any sponsorship related queries, do reach out to us</p>
     <div class="row align-items-center">
-     <div class="col-12 col-md-6 col-lg-4 contactcard">
+     <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
        <img src="img/karan.jpg" alt="">
        <h5 class="mt-2">Bharghav Choudhary</h5>
        <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
@@ -92,7 +93,7 @@
        <div class="phone">+91 9836726382</div>
      </div>
 
-     <div class="col-12 col-md-6 col-lg-4 contactcard">
+     <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
        <img src="img/karan.jpg" alt="">
        <h5 class="mt-2">Bharghav Choudhary</h5>
        <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
@@ -103,7 +104,7 @@
        <div class="name">bharghav@gmail.com</div>
        <div class="phone">+91 9836726382</div>
      </div>
-     <div class="col-12 col-md-6 col-lg-4 contactcard">
+     <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
        <img src="img/karan.jpg" alt="">
        <h5 class="mt-2">Bharghav Choudhary</h5>
        <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
