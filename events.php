553ff44c2dcd5a60c21d747e22ba1d76d57371a6
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-17 21:49:08 +0530

    Add files via upload

---
+++ events.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Events | Consortium'22 | E-CELL, VNIT</title>
+</head>
+<?php include('includes/header.php')?>
+<style>
+    .hexevent{
+        position: relative;
+        z-index:-1;
+        filter: brightness(80%);
+        margin-bottom: -1300px;
+        margin-left: 53%;
+    }
+    @media only screen and (max-width:767px) {
+        .hexevent{
+        position: relative;
+        z-index:-1;
+        filter: brightness(80%);
+        margin-bottom: -1300px;
+        margin-left: 10%;
+    }
+    }
+</style>
+<body style="background-color: #292929;">
+  <hr style="border-top: 1px solid #fff;opacity: 0.8;margin-top: 3px;">
+  <span><img src="img/hex.png" class="hexevent" alt=""></span>
+
+    <h1 class="text-white text-center mt-5" style="font-weight: bold;" >EVENTS</h1>
+    <div class="row align-items-center text-center mx-0 mb-3 text-white">
+        <div class="col-12 col-md-6 col-lg-4 eventcard mx-4">
+            <span class="tag px-4 py-2">PAN NIT</span>
+            <img src="img/karan.jpg" alt="">
+            <h5 class="mt-2" style="font-weight: bold;">WAR OF WORLDS</h5>
+              <h6 class="mt-2 mx-2 pb-3 text-white text-left desc ">War of Worlds is an exciting event that will engage you into a fast-paced virtual world and test your quick thinking, decision making and analytical skills.</h6>
+              <div class="col-md-12 text-right mb-5 mt-2">
+                <a href="" target="_blank"><button type="button" class="btn btn4">Register Now</button></a>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4 eventcard mx-4">
+            <!-- <span class="tag px-4 py-2">PAN NIT</span> -->
+            <img src="img/karan.jpg" alt="">
+            <h5 class="mt-2" style="font-weight: bold;">WAR OF WORLDS</h5>
+              <h6 class="mt-2 mx-2 pb-3 text-white text-left desc">War of Worlds is an exciting event that will engage you into a fast-paced virtual world and test your quick thinking, decision making and analytical skills.</h6>
+              <div class="col-md-12 text-right mb-5 mt-2">
+                <a href="" target="_blank"><button type="button" class="btn btn4">Register Now</button></a>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4 eventcard mx-4">
+            <!-- <span class="tag px-4 py-2">PAN NIT</span> -->
+            <img src="img/karan.jpg" alt="">
+            <h5 class="mt-2" style="font-weight: bold;">WAR OF WORLDS</h5>
+              <h6 class="mt-2 mx-2 pb-3 text-white text-left desc">War of Worlds is an exciting event that will engage you into a fast-paced virtual world and test your quick thinking, decision making and analytical skills.</h6>
+              <div class="col-md-12 text-right mb-5 mt-2">
+                <a href="" target="_blank"><button type="button" class="btn btn4">Register Now</button></a>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4 eventcard mx-4">
+            <span class="tag px-4 py-2">PAN NIT</span>
+            <img src="img/karan.jpg" alt="">
+            <h5 class="mt-2" style="font-weight: bold;">WAR OF WORLDS</h5>
+              <h6 class="mt-2 pb-3 mx-2 text-white text-left desc">War of Worlds is an exciting event that will engage you into a fast-paced virtual world and test your quick thinking, decision making and analytical skills.</h6>
+              <div class="col-md-12 text-right mb-5 mt-2">
+                <a href="" target="_blank"><button type="button" class="btn btn4">Register Now</button></a>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4 eventcard mx-4">
+            <!-- <span class="tag px-4 py-2">PAN NIT</span> -->
+            <img src="img/karan.jpg" alt="">
+            <h5 class="mt-2" style="font-weight: bold;">WAR OF WORLDS</h5>
+              <h6 class="mt-2 pb-3 mx-2 text-white text-left desc">War of Worlds is an exciting event that will engage you into a fast-paced virtual world and test your quick thinking, decision making and analytical skills.</h6>
+              <div class="col-md-12 text-right mb-5 mt-2">
+                <a href="" target="_blank"><button type="button" class="btn btn4">Register Now</button></a>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4 eventcard mx-4">
+            <!-- <span class="tag px-4 py-2">PAN NIT</span> -->
+            <img src="img/karan.jpg" alt="">
+            <h5 class="mt-2" style="font-weight: bold;">WAR OF WORLDS</h5>
+              <h6 class="mt-2 pb-3 mx-2 text-white text-left desc">War of Worlds is an exciting event that will engage you into a fast-paced virtual world and test your quick thinking, decision making and analytical skills.</h6>
+              <div class="col-md-12 text-right mb-5 mt-2">
+                <a href="" target="_blank"><button type="button" class="btn btn4">Register Now</button></a>
+            </div>
+          </div>
+    </div>
+
+
+  <span><img src="img/wireframe.png" alt="" width="1300px" style="position: relative;bottom: 1050px;z-index:-1;margin-bottom: -1500px;left: -10px;opacity: 0.7;"></span>
+
+  <div class="heading text-center my-5 container py-3" style="background-color: #3a3a3a;">
+    <h2>Contact Us</h2>
+    <p>For any sponsorship related queries, do reach out to us</p>
+    <div class="row align-items-center">
+     <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
+       <img src="img/karan.jpg" alt="">
+       <h5 class="mt-2">Bharghav Choudhary</h5>
+       <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
+       <h6 class="my-2 desig">TREASURER</h6>
+       <a href="https://www.linkedin.com/in/bhargav-choudhury-2371a7197/" target="_blank" >
+         <img id="linkedin" src="img/linkedin_black.png" alt="">
+       </a>
+       <div class="name">bharghav@gmail.com</div>
+       <div class="phone">+91 9836726382</div>
+     </div>
+
+     <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
+       <img src="img/karan.jpg" alt="">
+       <h5 class="mt-2">Bharghav Choudhary</h5>
+       <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
+       <h6 class="my-2 desig">TREASURER</h6>
+       <a href="https://www.linkedin.com/in/bhargav-choudhury-2371a7197/" target="_blank" >
+         <img id="linkedin" src="img/linkedin_black.png" alt="">
+       </a>
+       <div class="name">bharghav@gmail.com</div>
+       <div class="phone">+91 9836726382</div>
+     </div>
+     <div class="col-12 col-md-6 col-lg-4 contactcard mx-4">
+       <img src="img/karan.jpg" alt="">
+       <h5 class="mt-2">Bharghav Choudhary</h5>
+       <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
+       <h6 class="my-2 desig">TREASURER</h6>
+       <a href="https://www.linkedin.com/in/bhargav-choudhury-2371a7197/" target="_blank" >
+         <img id="linkedin" src="img/linkedin_black.png" alt="">
+       </a>
+       <div class="name">bharghav@gmail.com</div>
+       <div class="phone">+91 9836726382</div>
+     </div>
+    </div>
+   </div>
+
+
+<?php include('includes/footer.php')?>
+</body>
+</html>
